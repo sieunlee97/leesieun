e2be6807f30ee05fde73b4005d881e5881075f86
sieunlee <tldms0137@naver.com>
2020-11-27 11:43:04 +0900

    20201127 3교시 중간 커밋

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -28,7 +28,19 @@ public class HomeController {
 	 * 그래서 저장 시 콘솔에 보면, Reloading Context with...is completed 재실행되었다고 나온다.
 	 * 그래서 .java 클래스는 수정 후 약간 기다린 후 Reloading... 메시지 후 결과 확인 가능하다.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value="/", method=RequestMethod.GET) // 여기가 절대경로
+	public String index() {// 메소드(함수)의 리턴(출력)형태지정(String) 메소드명(index)() {구현내용}
+		// 상단 리퀘스트매핑에서 지정한 경로로 접근하게 되면, 아래에 있는 index() 메소드를 실행해라.
+		// 메소드명 제일 앞에 있는 public는 접근권한에 대한 키워드인데, 
+		// public(공공-다른 클래스에서 접근 가능), private(내부-현재 HomeController클래스에서만 접근 가능)
+		// public String index(){} 자바(스프링) 메소드 기본 형태
+		// $(document).ready(function(){});  jquery 기본형태
+		// 리턴반환값으로 사용한 String클래스는 문자열을 반환하고, 여기서는 indedx.jsp와 매칭되는 문자열 반환. 상대경로 필요
+		// 스프링MVC프로젝트에서 상대경로의 최상위는 views폴더. 최상위root폴더=views폴더
+		// 접두사(prefix), 접미사(suffix) index(접두사-파일명).jsp(접미사-확장자)
+		return "sample/index"; //접두사만 반환값으로 넣는다.
+	}
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("개발자들이 변수값 확인으로 사용하는 것이 로그이다. 현재 여러분 컴퓨터 언어는 {} 입니다.", locale);
 		logger.info("환영합니다. 현재 여러분의 컴퓨터 언어는 " + locale + " 입니다.");
@@ -46,6 +58,6 @@ public class HomeController {
 		System.out.println("현재 서버의 시간은 " + formattedDate + " 입니다.");
 		return "home"; // 결과적으로 return 반환값이 home(.jsp)에 연동됨.
 		// ★ 스프링에서 View단으로 데이터 이동(전송)에 대해서 살펴보았다.
-	}
+	}*/
 	
 }
