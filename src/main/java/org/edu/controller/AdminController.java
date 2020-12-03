c33a15cf6a1be57e83b26c504d8a7e128c6385cf
sieunlee <tldms0137@naver.com>
2020-12-03 12:51:27 +0900

    20201203 4교시 커밋

--- src/main/java/org/edu/controller/AdminController.java
+++ src/main/java/org/edu/controller/AdminController.java
@@ -1,8 +1,10 @@
 package org.edu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 스프링에서 사용가능한 클래스를 빈(bean)이라고 하고, @Controller 클래스를 사용하면 된다.
 @Controller
@@ -23,9 +25,16 @@ public class AdminController {
 	public String member_write() {
 		return "admin/member/member_write";
 	}
-
+	
+	//member_list.jsp에서 받은 데이터를 수신하는 역할  @RequestParam("키값") 리퀘스트파라미터 클래스 사용
+	//현재 컨트롤러 클래스에서 member_view.jsp로 데이터를 보내는 역할, Model 클래스 사용
+	//자료 흐름 : member_list.jsp -> (@RequestParam("user_id")-수신, Model-송신 -> member_view.jsp
 	@RequestMapping(value="/admin/member/member_view", method=RequestMethod.GET)
-	public String member_view() {
+	public String member_view(@RequestParam("user_id") String user_ID, Model model ) {
+		//member_list.jsp에서 받은 "user_id"를 model 클래스를 이용해서 "user_id2"라는 내용으로 user_ID에 저장한다.
+		//위에서 수신한 user_id를 member_view.jsp로 보낸다(아래)
+		model.addAttribute("user_id2", user_ID);
+		//member_view.jsp에서 model로부터 받은 데이터 user_id2를 출력하는 방법-점심이후
 		return "admin/member/member_view";		
 	}
 	
