2354f6c934cb216647c8fdceac417265203e9780
sieunlee <tldms0137@naver.com>
2020-12-14 12:50:07 +0900

    4교시 커밋

--- src/test/java/org/edu/test/DataSourceTest.java
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -2,6 +2,12 @@ package org.edu.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,7 +24,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
 @WebAppConfiguration
 public class DataSourceTest {
-
+	
+	@Inject
+	private DataSource dataSource; // 자바에서처럼 new 키워드로 객체 생성X, 스프링에서는 @Inject로 객체 생성	
+	
+	@Test
+	public void dbConnectinoTest() throws SQLException {
+		// 127.0.0.1의 3306포트에 있는 DB에 커넥션해서 사용가능한지 테스트
+		Connection connection = dataSource.getConnection();
+	}
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
