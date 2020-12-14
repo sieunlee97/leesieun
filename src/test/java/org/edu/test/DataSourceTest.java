057df59d86f44173ed86397d7cd1f4bf78bca02d
sieunlee <tldms0137@naver.com>
2020-12-14 14:03:40 +0900

    JUnit 테스트 중

--- src/test/java/org/edu/test/DataSourceTest.java
+++ src/test/java/org/edu/test/DataSourceTest.java
@@ -29,9 +29,19 @@ public class DataSourceTest {
 	private DataSource dataSource; // 자바에서처럼 new 키워드로 객체 생성X, 스프링에서는 @Inject로 객체 생성	
 	
 	@Test
-	public void dbConnectinoTest() throws SQLException {
+	public void dbConnectinoTest() throws Exception{
 		// 127.0.0.1의 3306포트에 있는 DB에 커넥션해서 사용가능한지 테스트
-		Connection connection = dataSource.getConnection();
+		
+		// try-catch 구문은 테스트에서만 사용하고, 
+		// 스프링에서는 throws Exception으로 예외를 스프링으로 보낸다.
+		try {
+			Connection connection = dataSource.getConnection();
+			System.out.println("DB접속 성공");
+		} catch (SQLException e) {
+			System.out.println("DB접속 실패 원인 : "+ e);
+			// e.printStackTrace();
+		}
+		
 	}
 	@Test
 	public void test() {
