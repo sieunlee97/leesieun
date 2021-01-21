41092f66d1906cfc79ea346598b981a584877840
이시은 <tldms0137@naver.com>
2021-01-21 11:56:14 +0900

    20210121 3교시 - 네아로 처리중

---
+++ src/main/java/org/edu/controller/NaverLoginController.java
@@ -0,0 +1,60 @@
+package org.edu.controller;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.edu.util.NaverLoginApi;
+import org.springframework.stereotype.Controller;
+
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.oauth.OAuth20Service;
+
+@Controller
+public class NaverLoginController {
+	
+	/**
+	 * 네이버API서버로 보내는 인증 요청문을 구성하는 파라미터(아래)
+	 * client_id: 네이버App 등록 후 발급받은 클라이언트 아이디
+	 * response_type: 네이버App에서 보내준 응답에 대한 데이터 타입
+	 * redirect_uri: 네이버 로그인 인증 결과를 전달 받는 콜백 URL
+	 * state: 네이버App이 생성한 토큰(네트워크에서 전송되는 자료의 단위-인증정보)의 상태
+	 */
+	
+	private final static String CLIENT_ID = "8vl0NWZbnTq1_R7BWOyX";
+	private final static String CLIENT_SECRET ="qIZzDwb0ju";
+	private final static String REDIRECT_URI = "http://127.0.0.1:8080/login";
+	private final static String SESSION_STATE = "oauth_state";
+	
+	/*프로필 조회 API URL - 사용자 이름 + 사용자 이메일 가져옴*/
+	private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
+	
+	/*네이버 아이디로 로그인 인증 url 생성 method*/
+	public String getAuthorizationUrl(HttpSession session) {
+		/*세션 유효성 검증을 위하여 난수를 생성 (아래)*/
+		String state = generateRandomString();
+		/*생성한 난수 값을 session변수에 저장 (아래)*/
+		setSession(session,state);
+		
+		/* Scribe 외부모듈에서 제공하는 인증 URL생성 기능을 이용하여 네아로 인증 URL생성 (아래) */ 
+		OAuth20Service oauthService = new ServiceBuilder()
+				.apiKey(CLIENT_ID)
+				.apiSecret(CLIENT_SECRET)
+				.callback(REDIRECT_URI)
+				.state(state)
+				.build(NaverLoginApi.instance()); //instance 바로 호출 static으로 정의 
+		
+		return oauthService.getAuthorizationUrl();
+	}
+
+	private void setSession(HttpSession session, String state) {
+		// http session 클래스에 데이터 저장
+		session.setAttribute(SESSION_STATE, state); //고유한 UUID값이 세션으로 저장
+	}
+
+	private String generateRandomString() {
+		// 세션 유효성 검증을 위한 난수 생성기
+		return UUID.randomUUID().toString();
+	}
+	
+}
--- src/main/java/org/edu/util/NaverLoginApi.java
+++ src/main/java/org/edu/util/NaverLoginApi.java
@@ -3,7 +3,7 @@ package org.edu.util;
 import com.github.scribejava.core.builder.api.DefaultApi20;
 
 public class NaverLoginApi extends DefaultApi20{
-
+	
 	@Override
 	public String getAccessTokenEndpoint() {
 		// OAuth2.0 인증 체크하는 네이버 REST-API 주소(아래) : 네이버가 만들어주는 것을 개발자가 가져다가 사용
@@ -16,5 +16,13 @@ public class NaverLoginApi extends DefaultApi20{
 		// 네이버 권한 체크하는 REST-API 주소(아래)
 		return "https://nid.naver.com/oauth2.0/authorize";
 	}
-
+	
+	// 싱글톤 인스턴스 객체를 생성하기 위해서 인스턴스 홀더 만든다.
+	private static class InstanceHolder {
+		private static final NaverLoginApi INSTANCE = new NaverLoginApi();
+	}
+	// 싱글톤으로 인스턴스 객체 생성하는 방식 : 클래스 실행을 1번만 하기 위해서
+	public static NaverLoginApi instance() {
+		return  InstanceHolder.INSTANCE;
+	}
 }
