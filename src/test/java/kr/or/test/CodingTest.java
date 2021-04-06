48485142fe2516e976dfa05815701e7d9b4bce03
이시은 <tldms0137@naver.com>
2021-04-06 10:08:46 +0900

    자바 코딩테스트 에러 수정

--- src/test/java/kr/or/test/CodingTest.java
+++ src/test/java/kr/or/test/CodingTest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CodingTest {
 	
-	String endString;
+	String endString="";
 	
 	public static void main(String[] args) {
 		CodingTest codingTest = new CodingTest();
@@ -22,34 +22,31 @@ public class CodingTest {
 	public String recursive(String w) {
 		if(w == null || w.equals("")) {
 			return w+endString;
-		} else {
-			int balancedIndex = getBalancedIndex(w); //w를 u,v로 파싱할 때 사용할 시작 인덱스값 추출
-			String u = w.substring(0, balancedIndex+1);
-			String v = w.substring(balancedIndex+1);
-			boolean isValidCheck = isValidString(u);//올바른 문자열인지 확인
-			if(isValidCheck == true) { //올바른 문자열이람녀
-				if(u.equals("(")) {
-					endString += ")";
-				}else {
-					u += recursive(v); //재귀함수 호추라면서 u값 누적
-					return u;
-				}
-			} else { //올바른 문자열이 아니라면
-				String createString="(";
-				createString += recursive(v); //재귀함수 호출하면서 createString값 누적
-				createString += ")";
-				createString += reArrange(u); //올바른 괄호문자열 만들기
-				return createString;
+		}
+		int balancedIndex = getBalancedIndex(w); //w를 u,v로 파싱할 때 사용할 시작 인덱스값 추출
+		String u = w.substring(0, balancedIndex+1);
+		String v = w.substring(balancedIndex+1);
+		boolean isValidCheck = isValidString(u);//올바른 문자열인지 확인
+		if(isValidCheck == true) { //올바른 문자열이라면
+			if(u.equals("(")) {
+				endString += ")";
 			}
-			return recursive(v);
+			u += recursive(v); //재귀함수 호추라면서 u값 누적
+			return u;
+		} else { //올바른 문자열이 아니라면
+			String createString="(";
+			createString += recursive(v); //재귀함수 호출하면서 createString값 누적
+			createString += ")";
+			createString += reArrange(u); //올바른 괄호문자열 만들기
+			return createString;
 		}
 	}
 	
 	//올바른 괄호문자열 만들기
-	private String reArrange(String u) {
+	public String reArrange(String u) {
 		String arrangeString = "";
 		for(int i=1; i<u.length()-1; i++) {
-			String tmpChar = u.substring(i, i+1);
+			String tmpChar = u.substring(i, i+1);//파싱주의 substring(시작인덱스, 끝인덱스-1)
 			if(tmpChar.equals("(")) {
 				arrangeString += ")";
 			}else if(tmpChar.equals(")")) {
@@ -62,29 +59,29 @@ public class CodingTest {
 	//올바른 문자열인지 확인
 	public boolean isValidString(String u) {
 		int balanceCount = 0;
-		for(int i=1; i<u.length()-1; i++){
-			String tmpChar = u.substring(i, i+1);
+		for(int i=0; i<u.length(); i++) {
+			String tmpChar = u.substring(i, i+1);//파싱주의 substring(시작인덱스, 끝인덱스-1)
 			if(tmpChar.equals("(")) {
 				balanceCount++;
 			}else if(tmpChar.equals(")")) {
 				balanceCount--;
 			}
+			if(balanceCount < 0) {
+				return false;
+			}
 		}
-		if(balanceCount < 0) {
-			return false;
-		}else {
-			return true;
-		}
+		return true;
 	}
+	
 	//w를 u,v로 파싱할 때 사용할 시작 인덱스값 추출
 	public int getBalancedIndex(String w) {
 		int index=0;
 		int balanceCount=0;
-		for(int i=1; i<w.length(); i++) {
+		for(int i=0; i<w.length(); i++) {
 			String tmpChar = w.substring(i, i+1); //substring(시작인덱스, 끝인덱스)
 			if(tmpChar.equals("(")) {
 				balanceCount++;
-			} else if(")" ==tmpChar){
+			} else if(tmpChar.equals(")")){
 				balanceCount--;
 			}
 			if(balanceCount ==0) {
@@ -92,6 +89,6 @@ public class CodingTest {
 				break;
 			}
 		}
-		return index;
+		return index; //균형이 맞지 않으면 0
 	}
 }
